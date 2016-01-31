64e2a71544e645efd8e06b147b9013fe217f45d4
Tyler Lutz <tlutz.computers@gmail.com>
2016-01-31 16:16:29 -0500

    Finished Example 1.6

--- example/app.js
+++ example/app.js
@@ -2,13 +2,22 @@
 	var app = angular.module('store',[]);
 	
 	app.controller('StoreController', function(){
-		this.product = gem;
+		this.products = gems;
 	});
 	
-	var gem = {
-		name: 'Dodecahedron',
-		price: 2.95.
-		description: '. . .',
-	}
-	
+	var gems = [
+		{
+			name: "Dodecahedron",
+			price: 2.95.
+			description: ". . .",
+			canPurchase = false,
+			soldOut: true,
+		},
+		{
+			name: "Pentagonal Gem",
+			price: 5.95,
+			description: ". . .",
+			canPurchase: false,
+		},
+	];
 })();
