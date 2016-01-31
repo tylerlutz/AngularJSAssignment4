dde9b576383c29f9363ce097528ee170e2e5547c
Tyler Lutz <tlutz.computers@gmail.com>
2016-01-31 16:25:45 -0500

    Finished Challenge 1.6

--- challenges/app.js
+++ challenges/app.js
@@ -1,9 +1,15 @@
 (function(){
-  var gem = { name: 'Azurite', price: 2.95 };
   var app = angular.module('gemStore', []);
   
   app.controller('StoreController',function(){
 	  this.product = gem;
   });
+  
+  var gem = {
+    name: 'Azurite',
+    price: 110.50,
+    canPurchase: false,
+    soldOut: true
+  };
   
 })();
