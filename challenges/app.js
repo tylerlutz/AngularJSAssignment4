6991b1a643b1ae29a227b1789ff7006b81a5720e
Tyler Lutz <tlutz.computers@gmail.com>
2016-01-31 21:58:48 -0500

    Completed Challenge 2.9

--- challenges/app.js
+++ challenges/app.js
@@ -1,6 +1,13 @@
 (function(){
   var app = angular.module('gemStore', []);
   
+  app.controller('GalleryController', function(){
+     this.current = 0;
+     this.setCurrent = function(newGallery){
+        this.current = newGallery || 0;
+     };
+  });
+  
   app.controller('StoreController',function(){
 	  this.products = gems;
   });
