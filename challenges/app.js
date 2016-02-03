a4090151674a6cb014581bf0f8ec206a5430ff10
Tyler Lutz <tlutz.computers@gmail.com>
2016-02-03 11:19:00 -0500

    Completed Challenge 4.7

--- challenges/app.js
+++ challenges/app.js
@@ -60,6 +60,20 @@
          controllerAs: "tab"
       };
    });
+   
+   app.directive("productGallery", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "product-gallery.html",
+      controller: function(){
+         this.current = 0;
+         this.setCurrent = function(imageNumber){
+         this.current = imageNumber || 0;
+       };
+      },
+      controllerAs: "gallery"
+    };
+  });
   
   var gems = [{
       name: 'Azurite',
