cce3a82df5065a38ba3062ee2e0c22725bb3c288
Tyler Lutz <tlutz.computers@gmail.com>
2016-02-03 11:26:16 -0500

    Completed Challenge 5.2

--- challenges/app.js
+++ challenges/app.js
@@ -1,5 +1,5 @@
 (function(){
-  var app = angular.module('gemStore', []);
+  var app = angular.module('gemStore', ['store-directives']);
   
   app.controller('GalleryController', function(){
      this.current = 0;
@@ -20,61 +20,7 @@
           this.review = {};
       };
   });
-  
-  app.directive('productDescription', function(){
-      return {
-         restrict: 'E',
-         templateUrl: 'product-description.html'
-      };
-  });
-  
-  app.directive('productSpecs', function(){
-      return {
-         restrict: 'A',
-         templateUrl: 'product-specs.html'
-      };
-  });
-  
-  app.directive("productReviews", function() {
-    return {
-      restrict: 'E',
-      templateUrl: "product-reviews.html"
-    };
-  });
-  
-  app.directive('productTabs', function(){
-      return {
-         restrict: 'E',
-         templateUrl: 'product-tabs.html',
-         controller: function(){
-            this.tab = 1;
-
-            this.isSet = function(checkTab) {
-                 return this.tab === checkTab;
-            };
-
-            this.setTab = function(activeTab) {
-                 this.tab = activeTab;
-             };
-         },
-         controllerAs: "tab"
-      };
-   });
-   
-   app.directive("productGallery", function() {
-    return {
-      restrict: 'E',
-      templateUrl: "product-gallery.html",
-      controller: function(){
-         this.current = 0;
-         this.setCurrent = function(imageNumber){
-         this.current = imageNumber || 0;
-       };
-      },
-      controllerAs: "gallery"
-    };
-  });
-  
+    
   var gems = [{
       name: 'Azurite',
       description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
